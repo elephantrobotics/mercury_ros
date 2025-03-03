72169079bdcc43b565a9792fcc07d1170b1cd120
wangWking <842749351@qq.com>
2025-03-03 16:16:18 +0800

    优化B1运动规划效果

--- mercury_b1_moveit/scripts/sync_plan.py
+++ mercury_b1_moveit/scripts/sync_plan.py
@@ -6,8 +6,8 @@ This file obtains the joint angle of the manipulator in ROS,
 and then sends it directly to the real manipulator using `pymycobot` API.
 This file is [slider_control.launch] related script.
 Passable parameters:
-      port1: Left arm serial port string. Default is "/dev/ttyTHS1"
-      port2: Right arm serial port string. Default is "/dev/ttyS0"
+      port1: Left arm serial port string. Default is "/dev/left_arm"
+      port2: Right arm serial port string. Default is "/dev/right_arm"
       Baud rate: Left and right arm serial port baud rate. The default value is 115200.
 """
 import math
@@ -17,60 +17,55 @@ from sensor_msgs.msg import JointState
 import traceback
 from pymycobot.mercury import Mercury
 
-# 限制频率的时间间隔
-UPDATE_INTERVAL = 0.1  # 10 Hz
-
-# 上一次更新的时间戳
-last_update_time = 0
-
 def callback(data):
-    global last_update_time
-
-    current_time = time.time()
-    if current_time - last_update_time < UPDATE_INTERVAL:
-        return  # 如果间隔时间不足，跳过此次更新
-
-    last_update_time = current_time
     # rospy.loginfo(rospy.get_caller_id() + "%s", data.position)
 
     data_list = []
     for index, value in enumerate(data.position):
         radians_to_angles = round(math.degrees(value), 2)
         data_list.append(radians_to_angles)
-        
+
     # print('data_list: {}'.format(data_list))
     left_arm = data_list[:7]
     right_arm = data_list[7:-3]
     middle_arm = data_list[-3:]
-    
-    print('left_angles: {}, right_angles: {}, middle_angles: {}'.format(left_arm, right_arm, middle_arm))
+    print('left_angles: {}'.format(left_arm))
+    print('right_angles: {}'.format(right_arm))
+    print('middle_angles: {}'.format(middle_arm))
 
-    try:
-        l.send_angles(left_arm, 80, _async=True)
-        r.send_angles(right_arm, 80, _async=True)
-        r.send_angle(11, middle_arm[0], 80, _async=True)
-        r.send_angle(12, middle_arm[1], 80, _async=True)
-        r.send_angle(13, middle_arm[2], 80, _async=True)
-    except Exception as e:
-        e = traceback.format_exc()
-        rospy.logerr(f"Failed to send angles: {e}")
+    l.send_angles(left_arm, 16, _async=True)
+    r.send_angles(right_arm, 16, _async=True)
+    r.send_angle(11, middle_arm[0], 16, _async=True)
+    r.send_angle(12, middle_arm[1], 16, _async=True)
+    r.send_angle(13, middle_arm[2], 16, _async=True)
 
 
 def listener():
     global l, r
     rospy.init_node("control_slider", anonymous=True)
 
-    l = Mercury("/dev/left_arm", 115200)
-    r = Mercury("/dev/right_arm", 115200)
-    l.set_movement_type(0) # 速度融合2 很耗时
-    l.set_movement_type(0)
+    port1 = rospy.get_param("~port1", "/dev/left_arm")
+    port2 = rospy.get_param("~port2", "/dev/right_arm")
+    baud = rospy.get_param("~baud", 115200)
+    print('left arm: {}, {}'.format(port1, baud))
+    print('right arm: {}, {}'.format(port2, baud))
+    l = Mercury(port1, baud)
+    r = Mercury(port2, baud)
+    time.sleep(0.05)
+    l.set_movement_type(2)
+    r.set_movement_type(2)
+    time.sleep(0.05)
+    l.set_vr_mode(1)
+    r.set_vr_mode(1)
+    time.sleep(0.05)
+    l.set_filter_len(3, 20)
+    r.set_filter_len(3, 20)
     time.sleep(0.05)
     rospy.Subscriber("joint_states", JointState, callback)
     # spin() simply keeps python from exiting until this node is stopped
     # spin()只是阻止python退出，直到该节点停止
     print("spin ...")
     rospy.spin()
-
 
 if __name__ == "__main__":
     listener()
